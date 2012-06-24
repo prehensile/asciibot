30fcf7bc3d754fd4ba0df25f3763e81abe14644f
Henry Cooke <me@prehensile.co.uk>
2012-06-24 15:50:25 +0100

    first github commit

---
+++ README.md
@@ -0,0 +1,4 @@
+asciibot
+========
+
+A simple bot which quotes lines from http://www.textfiles.com.
---
+++ asciibot.py
@@ -0,0 +1,142 @@
+from bs4 import BeautifulSoup
+import random
+import urllib2
+import re
+import tweepy
+import logging
+import os
+import sys
+
+class AsciiBot( object ):
+
+    def run( self, do_tweet=True ):
+
+        root_url = "http://textfiles.com"
+        root_paths = [ "100", "adventure", "apple", "bbs",
+        "computers", "conspiracy", "drugs", "etext", "food", "fun", "games", "groups",
+        "hacking", "hamradio", "holiday", "humor", "internet", "law", "magazines",
+        "media", "messages", "music", "news", "occult", "phreak", "piracy", "politics",
+        "programming", "reports", "rpg", "science", "sex", "sf", "stories", "survival",
+        "ufo", "uploads", "virus" ]
+
+        tweet = None
+        safety = 10
+        current_url = None
+        current_file = None
+        re_endpunctuation = re.compile("[.?!]+")
+        tweet_minlength = 15
+        tweet_maxlength = 140
+        while tweet is None and safety > 0:
+            
+            if current_url is None:
+                current_url = "%s/%s" % (root_url,random.choice( root_paths )) 
+
+            response = None
+            try:
+                response = urllib2.urlopen( current_url )
+                info = response.info()
+                content_type = info[ "Content-type" ]
+            except urllib2.HTTPError, e:
+                logging.info( e )
+
+            if response is None:
+                current_url = None
+
+            elif content_type == "text/plain":
+            
+                shortened_response = urllib2.urlopen(  "http://is.gd/create.php?format=simple&url=%s" % current_url )
+                shortened_url = shortened_response.read()
+               
+                text_file = response.read()
+
+                tweet_safety = 10
+                while tweet is None and tweet_safety > 0:
+                    matches = list( re_endpunctuation.finditer( text_file ) )
+                    match_ptr = random.randint(0,len(matches)-2)
+                    match_start = matches[ match_ptr ]
+                    match_end = matches[ match_ptr + 1 ]
+                    extract = text_file [ match_start.end() : match_end.end() ]
+                    extract = extract.lstrip()
+                    
+                    not_words = re.findall( "[^a-zA-Z0-9]", extract )
+                    # only continue if extract has a reasonable amount of alphanumerics
+                    # (not a bunch of punctuation/whitespace)
+                    if len( not_words ) < len( extract )* 0.3 and '"' not in extract: # supporting quotes is a PITA
+                        # refornat extract
+                        out = ""
+                        lines = extract.split("\n")
+                        for line in lines:
+                            line = line.rstrip( "\r -\t" ) # remove carriage returns, whitespace and hyphens
+                            line = line.lstrip( "> \t-" ) # remove wonky formatting
+                            if len(out)>0:
+                                out = "%s %s" % (out,line)
+                            else:
+                                out = line
+                        out = re.sub( "\s+", " ", out ) # collapse more than one space down to just one
+                        l = len(out)
+                        if l in xrange( tweet_minlength, 137 - len(shortened_url) - len(current_file) ):
+                            tweet = "%s // %s %s" % ( out, current_file.upper(), shortened_url )
+                    tweet_safety -= 1
+                if tweet_safety < 1:
+                    current_url = None
+            
+            elif content_type == "text/html":
+                html = response.read()
+                soup = BeautifulSoup( html )
+                links = soup.find_all( "a" )
+                
+                link = None
+                href = None
+                while href is None:
+                    link = random.choice( links )
+                    href = link.attrs[ "href" ]
+                    # skip links to things not in the current directory
+                    if href.startswith( "http://"):
+                        href = None
+
+                current_file = href 
+                current_url = "%s/%s" % (current_url,href)
+
+            else:
+                current_url = None
+
+            safety -=1
+
+        if tweet is not None:
+            logging.info( tweet )
+            if do_tweet:
+                creds_path = "twitter_creds"
+                error = None
+
+                try:
+                    fh = open( os.path.join( creds_path, 'consumer_token' ), 'r' )
+                    consumer_key, consumer_secret = fh.read().split("\n")
+                    fh.close()
+                except IOError, e:
+                    error = e
+
+                try: 
+                    fh = open( os.path.join( creds_path, 'access_token' ), 'r' )
+                    key, secret = fh.read().split("\n")
+                    fh.close()
+                except IOError, e:
+                    error = e
+
+                if error is None:
+                    auth = tweepy.OAuthHandler( consumer_key, consumer_secret )
+                    auth.set_access_token( key, secret )
+                    api = tweepy.API( auth )
+                    api.update_status( tweet )
+                
+                else:
+                    logging.warning( error )
+
+if __name__ == '__main__':
+    ## log to console
+    logging.basicConfig( 
+        stream=sys.stdout, 
+        level=logging.DEBUG, 
+        format='%(message)s'
+    )
+    bot = AsciiBot()
+    bot.run( do_tweet=False )
